83abf9dbca0c72c1b01f905cc61edbef45879f9d
davidjoaq <118952873+davidsosa2022@users.noreply.github.com>
2023-08-11 17:20:28 -0500

    Add files via upload

--- js/vino.js
+++ js/vino.js
@@ -7,3 +7,17 @@ span = document.getElementById("main-mii-name");
 
 img.src=vino.act_getMiiImage(activeUserSlot);
 span.innerText=vino.act_getName(activeUserSlot);
+
+function exit() {
+vino.soundPlayEx('SE_COMMON_FINISH', 1);
+vino.exit();
+}
+
+function back() {
+vino.soundPlayEx('SE_COMMON_FINISH', 1);
+history.back();
+}
+
+function miiClick() {
+vino.soundPlayEx('SE_FACE', 1);
+}
