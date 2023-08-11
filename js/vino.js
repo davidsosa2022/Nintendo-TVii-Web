024f3216657d275d7288f3e55438e6912d671225
davidjoaq <118952873+davidsosa2022@users.noreply.github.com>
2023-08-11 15:59:37 -0500

    Add files via upload

--- js/vino.js
+++ js/vino.js
@@ -7,32 +7,3 @@ span = document.getElementById("main-mii-name");
 
 img.src=vino.act_getMiiImage(activeUserSlot);
 span.innerText=vino.act_getName(activeUserSlot);
-
-  function lerp( a, b, alpha ) {
-    return a + alpha * ( b - a )
-  }
-
-
-  var lStickRightCheck = setInterval(function() {
-    wiiu.gamepad.update()
-    if(wiiu.gamepad.hold === 536870912) {
-      document.body.scrollLeft += lerp(-15, 15, (wiiu.gamepad.lStickX));
-    }
-
-  }, 10);
-
-  var rStickRightCheck = setInterval(function() {
-    wiiu.gamepad.update()
-    if(wiiu.gamepad.hold === 1073741824) {
-      document.body.scrollLeft += lerp(-15, -15, (wiiu.gamepad.lStickX));
-    }
-
-  }, 10);
-
-  var bButtonCheck = setInterval(function() {
-    wiiu.gamepad.update()
-    if(wiiu.gamepad.hold === 16384) {
-      history.back();
-    }
-
-  }, 50);
