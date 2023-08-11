fdcf8ef37060342624be967932d78974de4f89d9
davidjoaq <118952873+davidsosa2022@users.noreply.github.com>
2023-08-10 23:03:36 -0500

    Add files via upload

--- js/vino.js
+++ js/vino.js
@@ -2,8 +2,8 @@ vino.ir_enableCodeset(1); //enable the IR codeset
 
 //get the user Mii image and name
 const activeUserSlot = vino.act_getCurrentSlotNo();
-img = document.getElementById("mii-image");
-span = document.getElementById("mii-name");
+img = document.getElementById("main-mii-image");
+span = document.getElementById("main-mii-name");
 
 img.src=vino.act_getMiiImage(activeUserSlot);
 span.innerText=vino.act_getName(activeUserSlot);
